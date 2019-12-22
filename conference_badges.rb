6220f7441518fbaeb6241d3913857feaf604e018
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 18:37:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,10 +6,11 @@ end
 def batch_badge_creator
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   array.each do |speakers|
-    print "Hello, my name is (speakers)."
+    print "Hello, my name is #{speakers}."
   end
   speakers
 end
 
 def assign_rooms
+  
   
