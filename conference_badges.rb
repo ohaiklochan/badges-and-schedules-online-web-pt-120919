d48cd3f8c56970afea66ba5784beb2ad074eb7c7
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 19:18:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator(attendees)
   attendees = []
   attendees.each do |badges|
-  attendees.push "Hello! My name is #{attendees}."
+  attendees.push ("Hello! My name is #{attendees}.")
   end
   return attendees
 end
