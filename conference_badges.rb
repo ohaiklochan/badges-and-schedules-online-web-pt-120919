18ab55866874ce8c2ef3f5bb0492d859671bc3b6
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 18:29:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,4 +4,8 @@ def badge_maker(name)
 end
   
 def batch_badge_creator
-  
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  speakers.each do |badge|
+    print "Hello, my name is #{speakers}."
+  end
+end
