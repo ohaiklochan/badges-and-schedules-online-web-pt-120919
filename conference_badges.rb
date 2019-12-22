e870cc62a06c9a20c2c4bc811441a8455a1c0cbb
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 19:37:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
-  speakers.map.with_index |name, index|
+  speakers.map.with_index do |name, index|
     "Hello #{name}! You'll be assigned to room #{index + 1}!"
   end
 end  
